06528204f63d78372c791dcb3971893a2700c65a
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-27 21:23:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma method(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    elsif 
       
     puts "HUH?! SPEAK UP, SONNY!"
   elsif
